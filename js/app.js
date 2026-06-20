67928e556fa3131a546ba6cc8cf55fd06bd57646
murodjon0987 <wenzonepeser@gmail.com>
2026-06-20 16:18:27 +0500

    js

--- js/app.js
+++ js/app.js
@@ -1,23 +1,31 @@
-let btn = document.getElementById('btn');
-let users = document.getElementById('users');
+let btn = document.getElementById("btn");
+let product = document.getElementById("product");
 
-btn.addEventListener('click', () => {
-  fetch('https://dummyjson.com/products')
-    .then(res => res.json())
-    .then(data => {
-      users.innerHTML = data.products.map(
-        user => `
-          <h3>${user.price}</h3>
-          <h3>${user.title}</h3>
-          <h3>${user.rating}</h3>
-          <h3>${user.reviewerName}</h3>
-          <h3>${user.reviewerEmail}</h3>
-          <h3>${user.brand}</h3>
-          <h3>${user.date}</h3>
-          <h3>${user.reviewerEmail}</h3>
+btn.addEventListener("click", () => {
+
+  fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+
+      product.innerHTML = data.products
+        .map((item) => `
           
-        `
-      ).join('');
+          <div class="card">
+
+            <img src="${item.images[0]}">
+
+            <h2>${item.title}</h2>
+
+            <h3>${item.rating}</h3>
+
+            <p>${item.price}$</p>
+
+          </div>
+        
+        `)
+        .join("");
+
     })
-    .catch(err => console.error(err));
+    .catch((err) => console.log(err));
+
 });
